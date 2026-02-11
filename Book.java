be703b4927005632e6f612e98c2bc91307af40a4
varshavallamsetla <81659793+varshavallamsetla@users.noreply.github.com>
2026-02-11 14:10:34 -0800

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,65 @@
+public class Book
+{
+  private String title;
+  private String author;
+  private int rating;
+ 
+  /*** Constructor ****/
+  public Book(String t, String a)
+  {
+    title = t;
+    author = a;
+    rating = 0;
+  }
+ 
+   /*** Accessor methods ***/
+  public String getTitle() {
+    return title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+ 
+  public int getRating() {
+    return rating;
+  }
+
+ 
+  public String toString()
+  {
+    String info = "\"" + title + "\", written by " + author;
+    if (rating != 0)
+    {
+      info += ", rating is " + rating;
+    }
+    return info;
+  }
+
+  /*** Mutator methods ***/
+  public void setTitle(String t) {
+    title = t;
+  }
+
+  public void setAuthor(String a) {
+    author = a;
+  }
+
+  public void adjustRating(int r){
+    System.out.println("Adjusting rating by " + r);
+    if (r>=0 && r<=10){
+      rating=r;
+    }
+  }
+
+  public boolean equals(Book b){
+
+    if (this.title.equals(b.title)&& this.author.equals(b.author)){
+      return true;
+      }
+      else{
+        return false;
+      }
+  }
+
+}
